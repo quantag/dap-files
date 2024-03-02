115707e1b8a90f5fb44fc7ca829066f8624e7d02
AnatoliiLiabakh <129493174+AnatoliiLiabakh@users.noreply.github.com>
2024-03-02 10:39:36 +0200

    added loging and properties file
    added loging and properties file

--- src/main/java/com/quantag/DAP/Application.java
+++ src/main/java/com/quantag/DAP/Application.java
@@ -2,10 +2,40 @@ package com.quantag.DAP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
+    public static String mainFolder = null;
+
     public static void main(String[] args) {
+        try {
+            Properties prop = new Properties();
+            InputStream stream = new FileInputStream("dap-files.properties");
+            if(stream != null) {
+                prop.load(stream);
+                mainFolder = new String(prop.getProperty("folder"));
+            }
+            else {
+                log.error("ERROR loading dap-files.properties file");
+            }
+        }
+        catch (NullPointerException nullPointerException) {
+            log.error("ERROR loading dap-files.properties file: "+ nullPointerException.getMessage());
+        }
+        catch (IllegalArgumentException illegalArgumentException) {
+            log.error("ERROR loading dap-files.properties file: "+ illegalArgumentException.getMessage());
+        }
+        catch (IOException ioException) {
+            log.error("ERROR loading dap-files.properties file: "+ ioException.getMessage());
+        }
+
         SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/com/quantag/DAP/Controller.java
+++ src/main/java/com/quantag/DAP/Controller.java
@@ -3,41 +3,52 @@ package com.quantag.DAP;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
-//import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.commons.io.FilenameUtils;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.Base64;
 
 import com.quantag.DAP.model.*;
+import static com.quantag.DAP.Application.mainFolder;
 
 @RestController
-//@Slf4j
+@Slf4j
 public class Controller {
 
     @PostMapping("/submitFiles")
-    public void getSubmitFiles(@RequestBody GetSubmitFilesRequest requestData,
-                                HttpServletResponse response) {
-
-        //log.info("storeContact: " + requestData.getName() +", "+ requestData.getEmail());
-
-//        System.out.println("AL-> "+ requestData.getSessionId() +"  "+ requestData.getPath());
-//        System.out.println("AL-> "+ requestData.getSource());
+    public void getSubmitFiles(@RequestBody GetSubmitFilesRequest requestData, HttpServletResponse response) {
+        //log request data
+        String newRequest = "{" +
+                "\"sessionId\": \"" + requestData.getSessionId() + "\", " +
+                "\"path\": " + requestData.getPath() + ", " +
+                "\"source\": \"" + requestData.getSource() + "\"" +
+                "}";
+        log.info("request: "+ newRequest);
 
         if(requestData.getSessionId() != null && requestData.getPath() != null && requestData.getSource() != null) {
+            //gets file name from path
+            String fileName = FilenameUtils.getName( requestData.getPath() );
+            //decode source
             byte[] decodedSource = Base64.getDecoder().decode( requestData.getSource() );
-            try (OutputStream stream = new FileOutputStream( requestData.getPath() )) {
+
+            String pathToStore = (mainFolder == null) ? fileName : mainFolder + fileName;
+
+            try (OutputStream stream = new FileOutputStream(pathToStore)) {
                 stream.write(decodedSource);
+                log.info("Decoded to "+ pathToStore);
                 response.setStatus(HttpStatus.OK.value());
             }
             catch (Exception ex) {
-//                System.out.println("AL-> ERROR: "+ ex);
+                log.error(ex.getMessage());
                 response.setStatus(HttpStatus.CONFLICT.value());
             }
         }
         else {
+            log.error("BAD_REQUEST: NULL in request data");
             response.setStatus(HttpStatus.BAD_REQUEST.value());
         }
-
 
     }
 
